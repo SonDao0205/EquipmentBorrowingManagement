520af8cef296f92c78dba3963eac4b78b0713a88
Huy-Tuan <sieuka1990@gmail.com>
2026-04-16 17:24:39 +0700

    submit

--- src/main/java/com/practice/equipmentborrowingmanagement1/controller/EquipmentFormController.java
+++ src/main/java/com/practice/equipmentborrowingmanagement1/controller/EquipmentFormController.java
@@ -1,4 +1,86 @@
 package com.practice.equipmentborrowingmanagement1.controller;
 
+import com.practice.equipmentborrowingmanagement1.model.dto.EquipmentFormRequest;
+import com.practice.equipmentborrowingmanagement1.model.dto.EquipmentRequest;
+import com.practice.equipmentborrowingmanagement1.model.entity.Equipment;
+import com.practice.equipmentborrowingmanagement1.service.EquipmentFormService;
+import com.practice.equipmentborrowingmanagement1.service.EquipmentService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/equipment")
 public class EquipmentFormController {
+
+    @Autowired
+    private EquipmentFormService equipmentFormService;
+
+    @Autowired
+    private EquipmentService equipmentService;
+
+    @GetMapping("/borrow/{id}")
+    public String showBorrowForm(@PathVariable("id") int id, Model model) {
+
+        Equipment equipment = equipmentService.getEquipmentById(id);
+
+        if (equipment == null){
+            return "redirect:/equipment";
+        }
+
+        EquipmentFormRequest form = new EquipmentFormRequest();
+        form.setEquipmentId(id);
+
+        model.addAttribute("equipment", equipment);
+        model.addAttribute("equipmentFormRequest", form);
+
+        return "client/equipment-form";
+    }
+
+    @PostMapping("/submit")
+    public String submitForm(@Valid @ModelAttribute("equipmentFormRequest") EquipmentFormRequest equipmentFormRequest,
+                             BindingResult bindingResult,
+                             Model model,
+                             RedirectAttributes redirectAttributes) {
+
+        Equipment equipment = equipmentService.getEquipmentById(equipmentFormRequest.getEquipmentId());
+
+        if (equipment == null){
+            redirectAttributes.addFlashAttribute("error", "Thiết bị không tồn tại");
+            return "redirect:/equipment";
+        }
+
+        if (bindingResult.hasErrors()){
+            model.addAttribute("equipment", equipment);
+            return "client/equipment-form";
+        }
+
+        try {
+            equipmentFormService.processBorrow(equipmentFormRequest);
+        }catch (RuntimeException e){
+            bindingResult.rejectValue("quantity", "error.quantity", e.getMessage());
+            model.addAttribute("equipment", equipment);
+            return "client/equipment-form";
+        }
+
+        redirectAttributes.addFlashAttribute("success", "Mượn thiết bị thành công");
+
+        return "redirect:/equipment";
+    }
+
+    @PostMapping("delete")
+    public String delete(@Valid @RequestParam("equipId") int id, RedirectAttributes redirectAttributes) {
+        try{
+            equipmentFormService.deleteBorrow(id);
+            redirectAttributes.addFlashAttribute("success", "Xóa phiếu mượn thành công");
+        }catch (RuntimeException e){
+            redirectAttributes.addFlashAttribute("error", "Không tìm thấy phiếu mượn cần xóa");
+            return "redirect:/equipment/form";
+        }
+        return "redirect:/equipment";
+    }
 }
--- src/main/java/com/practice/equipmentborrowingmanagement1/service/EquipmentFormService.java
+++ src/main/java/com/practice/equipmentborrowingmanagement1/service/EquipmentFormService.java
@@ -1,4 +1,71 @@
 package com.practice.equipmentborrowingmanagement1.service;
 
+import com.practice.equipmentborrowingmanagement1.model.dto.EquipmentFormRequest;
+import com.practice.equipmentborrowingmanagement1.model.dto.EquipmentRequest;
+import com.practice.equipmentborrowingmanagement1.model.entity.Equipment;
+import com.practice.equipmentborrowingmanagement1.model.entity.EquipmentForm;
+import com.practice.equipmentborrowingmanagement1.repository.EquipmentFormRepository;
+import com.practice.equipmentborrowingmanagement1.repository.EquipmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EquipmentFormService {
+
+    @Autowired
+    private EquipmentRepository equipmentRepository;
+
+    @Autowired
+    private EquipmentFormRepository equipmentFormRepository;
+
+    public void processBorrow(EquipmentFormRequest form) {
+
+        Equipment equipment = equipmentRepository.findById(form.getEquipmentId());
+
+        if (equipment == null) {
+            throw new RuntimeException("Thiết bị không tồn tại");
+        }
+
+        if (form.getQuantity() <= 0) {
+            throw new RuntimeException("Số lượng phải lớn hơn 0");
+        }
+
+        if (form.getQuantity() > equipment.getStock()) {
+            throw new RuntimeException("Số lượng vượt quá tồn kho");
+        }
+
+        equipment.setStock(
+                equipment.getStock() - form.getQuantity()
+        );
+
+        equipmentRepository.update(equipment);
+
+        EquipmentForm request = new EquipmentForm();
+
+        request.setFullName(form.getFullName());
+        request.setStudentId(form.getStudentId());
+        request.setEmail(form.getEmail());
+        request.setQuantity(form.getQuantity());
+        request.setBorrowDate(form.getBorrowDate());
+        request.setReturnDate(form.getReturnDate());
+        request.setReason(form.getReason());
+        request.setEquipment(equipment);
+
+        equipmentFormRepository.save(request);
+    }
+
+    public void deleteBorrow(int id) {
+        EquipmentForm request = equipmentFormRepository.findById(id);
+        if (request == null) {
+            throw new RuntimeException("Không tìm thấy phiếu cần xóa");
+        }
+
+        Equipment equipment = request.getEquipment();
+
+        if (equipment == null){
+            throw new RuntimeException("Thiết bị không tồn tại");
+        }
+        equipment.setStock(equipment.getStock() + request.getQuantity());
+        equipmentRepository.update(equipment);
+    }
 }
